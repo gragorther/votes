a91f72c423346f663e552b073a606fa95109348a
gregor <gregor@gregtech.eu>
2025-04-27 15:17:07 +0200

    make the cursor style appear on the entire page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -67,80 +67,81 @@
 </script>
 
 <title>Lemvotes</title>
+<div class={cursor}>
+	<div class="text-center">
+		<span class=" flex justify-center"
+			><img
+				class="display:inline inline size-10"
+				src="/favicon.png"
+				alt="website icon, a downvote and upvote arrow on the left and the lemmy logo on the right"
+			/>
 
-<div class="{cursor} text-center">
-	<span class=" flex justify-center"
-		><img
-			class="display:inline inline size-10"
-			src="/favicon.png"
-			alt="website icon, a downvote and upvote arrow on the left and the lemmy logo on the right"
-		/>
-
-		<h1 class="text-center text-4xl">Lemvotes</h1></span
-	>
-
-	<p>A tool I made for getting information about who votes on which Lemmy post/comment</p>
-	<div class="flex items-center justify-center">
-		<div
-			class="max-w-xs content-center justify-center overflow-auto rounded-2xl border-2 border-pink-400 p-4"
+			<h1 class="text-center text-4xl">Lemvotes</h1></span
 		>
-			<form
-				on:submit|preventDefault={handleSubmit}
-				class="mt-2 flex flex-col flex-wrap justify-center gap-2"
+
+		<p>A tool I made for getting information about who votes on which Lemmy post/comment</p>
+		<div class="flex items-center justify-center">
+			<div
+				class="max-w-xs content-center justify-center overflow-auto rounded-2xl border-2 border-pink-400 p-4"
 			>
-				<input
-					type="text"
-					bind:value={postId}
-					placeholder="Enter {post_type} URL"
-					class="self-stretch rounded-md border-2 border-orange-500 text-center"
-					on:focus={(e) => {
-						const input = e.target as HTMLInputElement | null;
-						if (input) {
-							input.select();
-						}
-					}}
-					required
-				/><span>
-					<input type="checkbox" bind:checked={comment} id="post-or-comment" />
-					<label class="select-none" for="post-or-comment">This is a comment</label>
-				</span>
-				<button
-					type="submit"
-					class="{buttonCursor} self-center rounded {buttonColor} px-4 py-2 text-white"
-					{disabled}
+				<form
+					on:submit|preventDefault={handleSubmit}
+					class="mt-2 flex flex-col flex-wrap justify-center gap-2"
 				>
-					Get Votes
-				</button>
-			</form>
-			<div class="mt-2 text-left">
-				<p>
-					This tool is open source, you can access the code <a
-						href="https://github.com/gragorther/votes">on GitHub</a
-					>.
-				</p>
+					<input
+						type="text"
+						bind:value={postId}
+						placeholder="Enter {post_type} URL"
+						class="self-stretch rounded-md border-2 border-orange-500 text-center"
+						on:focus={(e) => {
+							const input = e.target as HTMLInputElement | null;
+							if (input) {
+								input.select();
+							}
+						}}
+						required
+					/><span>
+						<input type="checkbox" bind:checked={comment} id="post-or-comment" />
+						<label class="select-none" for="post-or-comment">This is a comment</label>
+					</span>
+					<button
+						type="submit"
+						class="{buttonCursor} self-center rounded {buttonColor} px-4 py-2 text-white"
+						{disabled}
+					>
+						Get Votes
+					</button>
+				</form>
+				<div class="mt-2 text-left">
+					<p>
+						This tool is open source, you can access the code <a
+							href="https://github.com/gragorther/votes">on GitHub</a
+						>.
+					</p>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
-{#if error}
-	<p class="mt-4 text-center text-red-500">{error}</p>
-{/if}
+	{#if error}
+		<p class="mt-4 text-center text-red-500">{error}</p>
+	{/if}
 
-{#if !error}
-	<div class="m-2 flex justify-center break-all">
-		<ul class="mt-4 space-y-2">
-			{#each [...votes].sort((a, b) => a.vote - b.vote) as vote}
-				<li class="rounded border p-2">
-					<a href="https://{vote.instance}/u/{vote.user}" class="text-blue-600"
-						>{vote.user}@{vote.instance}</a
-					>
-					{#if vote.vote === -1}
-						<ArrowBigDown color="#eb3434" />
-					{:else}
-						<ArrowBigUp color="#34a4e0" />
-					{/if}
-				</li>
-			{/each}
-		</ul>
-	</div>
-{/if}
+	{#if !error}
+		<div class="m-2 flex justify-center break-all">
+			<ul class="mt-4 space-y-2">
+				{#each [...votes].sort((a, b) => a.vote - b.vote) as vote}
+					<li class="rounded border p-2">
+						<a href="https://{vote.instance}/u/{vote.user}" class="text-blue-600"
+							>{vote.user}@{vote.instance}</a
+						>
+						{#if vote.vote === -1}
+							<ArrowBigDown color="#eb3434" />
+						{:else}
+							<ArrowBigUp color="#34a4e0" />
+						{/if}
+					</li>
+				{/each}
+			</ul>
+		</div>
+	{/if}
+</div>
