1bb9ce9a29bf6b8aaf03ebb9679bab0c82c412a7
gregor <gregor@gregtech.eu>
2025-05-04 14:31:05 +0200

    add http cats :3

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<title>Lemvotes - {page.error.message}</title>
+<img
+	class="self-center"
+	src="https://http.cat/{page.status}"
+	alt="Error: {page.error.message}, {page.status}"
+/>
