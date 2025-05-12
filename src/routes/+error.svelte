746b3879e9974625e0e6d7d7b369f8f718d31ae8
gregor <gregor@gregtech.eu>
2025-05-12 15:44:11 +0200

    uplaoad

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,9 +2,9 @@
 	import { page } from '$app/state';
 </script>
 
-<title>Lemvotes - {page.error.message}</title>
+<title>Lemvotes - {page.error!.message}</title>
 <img
 	class="self-center"
 	src="https://http.cat/{page.status}"
-	alt="Error: {page.error.message}, {page.status}"
+	alt="Error: {page.error!.message}, {page.status}"
 />
