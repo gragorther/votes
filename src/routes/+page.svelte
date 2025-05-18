00bcff16634ee9751929d1aa7fabc11b3c5ce9b6
gregor <gregor@gregtech.eu>
2025-05-18 19:40:39 +0200

    add loading spinner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { Loader } from 'lucide-svelte';
 	let query: string = $state('');
 	let query_type = $state('post');
 
@@ -15,10 +16,14 @@
 		}
 	});
 
-	let redirect_url = $derived(`/${query_type}/${query}`.replace(/https:\/\//g, ''));
+	let redirectUrl = $derived(`/${query_type}/${query}`.replace(/https:\/\//g, ''));
+
+	let loading = $state(false);
+	let buttonColor = $state('bg-blue-500');
 
 	function onSubmit(event: any) {
-		event.preventDefault();
+		buttonColor = 'bg-gray-400';
+		loading = true;
 	}
 </script>
 
@@ -29,50 +34,56 @@
 		<p>Use the following form to get votes</p>
 		<form
 			class="mt-2 flex flex-col flex-wrap justify-center gap-2 select-none"
-			action={redirect_url}
+			action={redirectUrl}
 			method="GET"
+			onsubmit={onSubmit}
 		>
-			<input
-				type="text"
-				bind:value={query}
-				placeholder={input_placeholder}
-				class="self-stretch rounded-md border-2 border-orange-500 text-center"
-				onfocus={(e) => {
-					const input = e.target as HTMLInputElement | null;
-					if (input) {
-						input.select();
-					}
-				}}
-				required
-			/><span>
-				<fieldset>
-					<input
-						type="radio"
-						id="post"
-						name="submit-type"
-						value="post"
-						checked
-						bind:group={query_type}
-					/>
-					<label for="post">Post</label>
-					<input
-						type="radio"
-						id="comment"
-						name="submit-type"
-						value="comment"
-						bind:group={query_type}
-					/>
-					<label for="comment">Comment</label>
-					<input type="radio" id="user" name="submit-type" value="user" bind:group={query_type} />
-					<label for="user">User</label>
-				</fieldset>
-			</span>
-			<button
-				type="submit"
-				class="cursor-pointer self-center rounded bg-blue-500 px-4 py-2 text-white"
-			>
-				Get Votes
-			</button>
+			<fieldset disabled={loading}>
+				<input
+					type="text"
+					bind:value={query}
+					placeholder={input_placeholder}
+					class="self-stretch rounded-md border-2 border-orange-500 text-center"
+					onfocus={(e) => {
+						const input = e.target as HTMLInputElement | null;
+						if (input) {
+							input.select();
+						}
+					}}
+					required
+				/><span>
+					<fieldset>
+						<input
+							type="radio"
+							id="post"
+							name="submit-type"
+							value="post"
+							checked
+							bind:group={query_type}
+						/>
+						<label for="post">Post</label>
+						<input
+							type="radio"
+							id="comment"
+							name="submit-type"
+							value="comment"
+							bind:group={query_type}
+						/>
+						<label for="comment">Comment</label>
+						<input type="radio" id="user" name="submit-type" value="user" bind:group={query_type} />
+						<label for="user">User</label>
+					</fieldset>
+				</span>
+				<button
+					type="submit"
+					class="cursor-pointer self-center rounded {buttonColor} px-4 py-2 text-white"
+				>
+					Get Votes
+				</button>
+				{#if loading}
+					<Loader class="inline animate-spin" />
+				{/if}
+			</fieldset>
 		</form>
 	</div>
 </div>
