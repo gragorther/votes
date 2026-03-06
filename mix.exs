17029c1accb5b6523ce692831d811df59ca14049
gregor <gregor@gregtech.eu>
2026-03-06 20:32:07 +0100

    deps

--- mix.exs
+++ mix.exs
@@ -66,7 +66,8 @@ defmodule Votes.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:json_ld, ">= 1.0.0"}
+      {:json_ld, ">= 1.0.0"},
+      {:timex, "> 3.0.0"}
     ]
   end
 
